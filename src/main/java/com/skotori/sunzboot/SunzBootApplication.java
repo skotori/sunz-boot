d47616b7ca73fe5ff74126e9dd91f9cb44b48bcc
sz <1270810639@qq.com>
2019-11-05 14:52:34 +0800

    创建项目

---
+++ src/main/java/com/skotori/sunzboot/SunzBootApplication.java
@@ -0,0 +1,13 @@
+package com.skotori.sunzboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunzBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunzBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skotori/sunzboot/SunzBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skotori.sunzboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunzBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
